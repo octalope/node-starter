9adf9685cb5cb29323340a847ca00d10078805cb
Steve Brooks <sbrooks@accusoft.com>
2025-02-11 14:05:53 -0500

    update setup.js to work with node 22

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
+import fs from 'node:fs';
 
-import packageJson from './package.json' assert {type: 'json'};
+import packageJson from './package.json' with {type: 'json'};
 
 packageJson.name = process.argv[2];
 packageJson.version = '1.0.0';
